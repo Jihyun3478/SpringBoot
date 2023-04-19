1d53de9a33c61e51d7fbbb3b92955b0552091797
Jihyun3478 <leejihyun3478@gmail.com>
2023-04-19 21:15:04 +0900

    update 2023.04.19

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -11,24 +11,30 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 
 // AppConfig를 사용하면 Application의 사용 영역과 구성 영역을 분리할 수 있음
 public class AppConfig {
+
+    // @Bean -> memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
     // 스프링 컨테이너에 등록
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
