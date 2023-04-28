91d7a5ed8c9ba589f1186a1d236084438af8189f
Jihyun3478 <leejihyun3478@gmail.com>
2023-04-28 23:42:12 +0900

    update 2023.04.28

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -21,8 +21,8 @@ public class AllBeanTest {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
         DiscountService discountService = ac.getBean(DiscountService.class);
         Member member = new Member(1L, "userA", Grade.VIP);
-        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
 
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
         assertThat(discountService).isInstanceOf(DiscountService.class);
         assertThat(discountPrice).isEqualTo(1000);
 
