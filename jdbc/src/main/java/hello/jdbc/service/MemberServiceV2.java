32aa3001da1a93eba13c21f286027bf2b3472ec1
Jihyun3478 <leejihyun3478@gmail.com>
2023-05-18 20:09:07 +0900

    update 2023.05.18

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,56 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/*
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Connection con = dataSource.getConnection();
+        try{
+            con.setAutoCommit(false); // 트랜잭션 시작
+            // 비즈니스 로직
+            Member fromMember = memberRepository.findById(fromId);
+            Member toMember = memberRepository.findById(toId);
+
+            memberRepository.update(fromId, fromMember.getMoney() - money);
+            validation(toMember);
+            memberRepository.update(toId, toMember.getMoney() + money);
+            con.commit(); // 성공 시 커밋
+        } catch(Exception e) {
+            con.rollback(); // 실패 시 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            if(con != null) {
+                try {
+                    con.cl
+                } catch (Exception e) {
+                    log.info("error", e);
+                }
+            }
+        }
+
+    }
+
+    private static void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
