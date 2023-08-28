004dbb949494347620db23a6fa0b18a1f90a875f
Jihyun3478 <leejihyun3478@gmail.com>
2023-08-28 16:58:13 +0900

    update 2023.08.28 복습

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    // JPQL : select m from Member m where m.name = ? 쿼리를 자동으로 구현해줌
+    // Optional<Member> findByName(String name, Long id); => select m from Member m where m.name = ? m.id = ?
     @Override
     Optional<Member> findByName(String name);
 }
