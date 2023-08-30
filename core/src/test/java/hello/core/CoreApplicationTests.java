81dd978080268dc6b8a861034983968605d50ff0
Jihyun3478 <leejihyun3478@gmail.com>
2023-08-30 19:17:29 +0900

    update 2023.08.30 복습

--- core/src/test/java/hello/core/CoreApplicationTests.java
+++ core/src/test/java/hello/core/CoreApplicationTests.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest // 스프링이 자동으로 통합 테스트 실행
+@SpringBootTest
 class CoreApplicationTests {
 
 	@Test
