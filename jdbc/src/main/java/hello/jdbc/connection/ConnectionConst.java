005caf7efc49e94184637801be4f0059af9e5f89
Jihyun3478 <leejihyun3478@gmail.com>
2023-09-22 00:44:33 +0900

    2023.09.22JDBC 복습

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public class ConnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc?serverTimezone=UTC";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "hoon@0815!!";
+}
